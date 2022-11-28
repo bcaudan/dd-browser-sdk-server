0d344d2f1ad6ac97db593feccec13dda9b211ac1
Bastien Caudan <bastien.caudan@datadoghq.com>
2022-11-28 14:28:02 +0100

    add public dir

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const express = require('express')
 const port = process.env.PORT || 5000;
 const app = express()
 
-// app.use(express.static(path.join(__dirname, '../sandbox')))
+app.use(express.static(path.join(__dirname, './public')))
 
-app.get('/', (_, res) => {
-  res.send('hello world!')
+app.get('/status', (_, res) => {
+  res.send('ok')
 })
 
 app.listen(port, () => console.log(`Server listening on port ${port}.`))
