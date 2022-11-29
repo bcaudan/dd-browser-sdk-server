c38a640263012dadb9510e8af2e0fd0cae13371b
Bastien Caudan <bastien.caudan@datadoghq.com>
2022-11-29 10:36:23 +0100

    add /ok?duration=x

--- server/backend.js
+++ server/backend.js
@@ -2,6 +2,11 @@ function withBackend(app) {
   app.get('/error', function (req, res) {
       res.sendStatus(500)
   })
+
+  app.get('/ok', (req, res) => {
+    const timeoutDuration = req.query.duration ? Number(req.query.duration) : 0
+    setTimeout(() => res.send('ok'), timeoutDuration)
+  })
 }
 
 module.exports = { withBackend }
